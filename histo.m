41108e7fcea6280a78188e0322b70aca09369430
Rianmaulana23 <66192792+Rianmaulana23@users.noreply.github.com>
2020-05-31 11:32:42 +0700

    Add files via upload
    uas pcd

---
+++ histo.m
@@ -0,0 +1,15 @@
+function histo(Img)
+  
+  [jum_baris, jum_kolom] = size (Img);
+  Img = double(Img);
+  
+  Histog = zeros(256, 1);
+  for baris=1 : jum_baris
+    for kolom=1 : jum_kolom
+      Histog(Img(baris, kolom)+1) = ...
+          Histog(Img(baris, kolom)+1) + 1;
+    end
+  end
+  
+  Horis = (0:255)';
+  bar(Horis, Histog);
---
+++ kebiner.m
@@ -0,0 +1,16 @@
+img = imread('D:\MATERI KULIAH\CitraDigital\UAS\jarak.jpg');
+[tinggi, lebar] = size(img);
+
+ambang = 210
+biner = zeros(tinggi, lebar);
+for baris=1 : tinggi
+  for kolom=1 : lebar
+    if img(baris, kolom) >= ambang
+      biner(baris, kolom) = 0;
+    else
+      biner(baris, kolom) = 1;
+    end
+  end
+end
+
+imshow(biner);
